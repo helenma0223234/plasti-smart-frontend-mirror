5a64534116304ec7a7891f7134dd1560e53b06d0
aneeshp4 <69907361+aneeshp4@users.noreply.github.com>
2024-02-04 14:22:07 -0500

    app opens to the camera now and renders the camera

--- src/screens/BaseScreens/CameraPage/index.tsx
+++ src/screens/BaseScreens/CameraPage/index.tsx
@@ -1,28 +1,21 @@
-import React, { FC } from "react";
-import { Camera, useCameraDevice } from "react-native-vision-camera";
-import { StyleSheet } from "react-native";
-import { SafeAreaView } from "react-native";
+import React, { FC, useState } from "react";
+import { Camera, CameraType } from 'expo-camera';
+import { View } from "react-native";
 import FormatStyle from "../../../utils/FormatStyle";
 
 // import * as tf from "react-native-tensorflow";
 
-const CameraPage = () => {
-  const device = useCameraDevice("back", {
-    physicalDevices: ["wide-angle-camera"],
-  });
+const CameraPage: FC = () => {
+  const [status, requestPermission] = Camera.useCameraPermissions();
 
   return (
-    <SafeAreaView style={FormatStyle.container}>
-      {device == null ? (
-        <h1>No camera</h1>
-      ) : (
-        <Camera
-          style={StyleSheet.absoluteFill}
-          device={device}
-          isActive={true}
-        />
-      )}
-    </SafeAreaView>
+    <View style={FormatStyle.container}>
+      <Camera
+        style={{width: '50%', height: '50%'}}
+        type={CameraType.back}
+        ref={requestPermission}
+      />
+    </View>
   );
 };
 

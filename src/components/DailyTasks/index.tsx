84d7ce7f401e5325c3d639bc58bdc489f98cf50a
Yuchuan (Helen) Ma <69632858+helenma0223234@users.noreply.github.com>
2024-05-13 14:53:32 -0400

    new home page

--- src/components/DailyTasks/index.tsx
+++ src/components/DailyTasks/index.tsx
@@ -1,10 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import FormatStyle from 'utils/FormatStyle';
 import TextStyles from 'utils/TextStyles';
 import Colors from 'utils/Colors';
-import { ScrollView } from 'react-native-gesture-handler';
 import { Text, View } from 'react-native';
 import CompletedDot from 'components/CompletedDot';
+import { Dimensions } from 'react-native';
+
+const screenWidth = Dimensions.get('window').width;
+const screenHeight = Dimensions.get('window').height;
 
 interface DailyTasksProps {
   taskCompletionStatuses?: boolean[]
@@ -15,7 +18,7 @@ const DailyTasks = ({ taskCompletionStatuses = [] }: DailyTasksProps) => {
   const dotColors = [Colors.secondary.red, Colors.secondary.yellow, Colors.primary.dark];
   return (
     <View>
-      <View style={{ gap: 10, marginBottom: 30 }}>
+      <View style={{ gap: 10, marginBottom: 10 }}>
         {taskCompletionStatuses.map((taskComplete, index) => (
           <Task key={index} complete={taskComplete} title={taskTitles[index]} color={dotColors[index]}/>
         ))}
@@ -32,7 +35,7 @@ interface TaskProps {
 
 const Task = ({ complete, title, color }: TaskProps) => {
   return (
-    <View style={{ borderColor: Colors.primary.dark, borderWidth: 2, padding: 12, borderRadius: 15, width: 350, flexDirection: 'row', justifyContent: 'space-between' }}>
+    <View style={{ borderColor: Colors.primary.dark, borderWidth: 2, padding: 20, borderRadius: 15, width: screenWidth * 0.89, flexDirection: 'row', justifyContent: 'space-between' }}>
       <Text style={{ ...TextStyles.regular }}>{title}</Text>
       <CompletedDot color={color} completed={complete}></CompletedDot>
     </View>

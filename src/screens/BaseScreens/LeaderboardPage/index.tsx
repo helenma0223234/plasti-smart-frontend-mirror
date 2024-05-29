7096a4928aaac6c6da1ac4c3ce9cbd132ab49f42
Yuchuan (Helen) Ma <69632858+helenma0223234@users.noreply.github.com>
2024-05-29 18:29:57 -0400

    Update index.tsx

--- src/screens/BaseScreens/LeaderboardPage/index.tsx
+++ src/screens/BaseScreens/LeaderboardPage/index.tsx
@@ -63,7 +63,7 @@ const LeaderboardPage = () => {
       };
 
       fetchLeaderboard();
-    }, [])
+    }, []),
   );
 
   // const userRank = user?.rank || 100;
@@ -233,7 +233,7 @@ const LeaderboardPage = () => {
           {userRank > 20 && 
       <View style={{
         position: 'absolute',
-        top: screenHeight * 0.34,
+        top: screenHeight * 0.32,
         left: -screenWidth * 0.04,
         width: '110%',
         height: screenHeight * 0.062,

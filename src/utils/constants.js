5eb8d0811589887307118f863f8cabd9ad228962
aneeshp4 <69907361+aneeshp4@users.noreply.github.com>
2024-06-04 23:47:13 -0400

    updated replicate token

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,6 +2,6 @@
 export const SERVER_URL = 'http://192.168.7.228:8080/';
 
 // For replicate image recognition model
-export const REPLICATE_API_TOKEN = 'r8_Z4cegUDffPG70CxKOAuucerGC8zdnAk1gKs2u';
+export const REPLICATE_API_TOKEN = 'r8_5pVitArqb71gdhh8gsjzA13tzuqt0VF016DkI';
 export const REPLICATE_URL = 'https://api.replicate.com/v1/predictions';
 export const REPLICATE_VERSION = 'ae36115eefe7537542f19415b69eb1fe19cca4694c113d000b3ffe765452dde4';

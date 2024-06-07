d3da34a1efa98c74c507c9bb05c518c78dffc843
aneeshp4 <69907361+aneeshp4@users.noreply.github.com>
2024-06-07 16:24:25 -0400

    added .env file

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 // With Expo Go, SERVER_URL needs to be exact IPv4 Address
-export const SERVER_URL = 'http://192.168.7.228:8080/';
+export const SERVER_URL = process.env.EXPO_PUBLIC_SERVER_URL;
 
 // For replicate image recognition model
-export const REPLICATE_API_TOKEN = 'r8_5pVitArqb71gdhh8gsjzA13tzuqt0VF016DkI';
+export const REPLICATE_API_TOKEN = process.env.EXPO_PUBLIC_REPLICATE_API_TOKEN;
 export const REPLICATE_URL = 'https://api.replicate.com/v1/predictions';
-export const REPLICATE_VERSION = '88f4ff764bf08d2fa6b5dc45dc79ac569a3ca1f107dcbb29dc2db8220fbe8358';
+export const REPLICATE_VERSION = process.env.EXPO_PUBLIC_REPLICATE_VERSION;
